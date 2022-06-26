6ea98f5d55f3cb1e88b9f0430073f75666df75e6
Pedro <pedrovalverde747@gmail.com>
2022-06-26 10:15:07 -0300

    enter

---
+++ view/shop.php
@@ -0,0 +1,11 @@
+<?php include_once('header.php'); ?>
+<section>
+    <div class="container">
+        <div id="destaque-produto">
+            <div class="item">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once('footer.php'); ?>
